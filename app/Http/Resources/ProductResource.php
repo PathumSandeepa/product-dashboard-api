f460bb51721d10e171eb2e91e248ce6307cc5cdc
PathumSandeepa <pathumsandeepa13@gmail.com>
2026-02-10 14:39:25 +0530

    [api-05] add ProductResource for API response formatting in ProductController

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+            'category' => $this->category,
+            'image' => $this->image,
+            'rating' => $this->rating,
+        ];
+    }
+}
